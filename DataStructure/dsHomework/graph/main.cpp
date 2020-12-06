cfe06cd0df21eb4bd3e6bb772587afce1735eeef
LukeLIN-web <1263810658@qq.com>
2020-12-06 19:05:24 +0800

    add some explanation

--- DataStructure/dsHomework/graph/main.cpp
+++ DataStructure/dsHomework/graph/main.cpp
@@ -21,23 +21,34 @@ vector<int>graph[1005];//整个图，其中graph[i][j]表示结点i的第j条边
 首先找找这个边在不在无序map, 如果不在 ,那就insert一个 , 通过tran的size,就可以获得他的大小
 利用构造函数, 在edges中创建边.  同时在 图中插入边,以及初始化一个反向边.
 */
-void insertEdge(string&from,string&to,int cap){//插入边，起点为from，终点是to，容量为cap
-
-}
+void insertEdge(string&from,string&to,int cap);
 /*
 1. 首先把所有残余网络初始化为0,广度优先遍历查找从源点到达汇点的增广路
 然后定义一个数组 a,a[i]代表从源点到i的最大流通量，即每次源点开始搜索，
 最后的a[n]就是从源点到汇点的最大流通量，
+ 首先把source 入队, source 广度优先搜索, 先遍历从source出发的第一条边.
+ 起点的残量置为无穷大
 2.遍历x节点的每一条边.如果当前边的终点的残量为0且容量大于流量,
-那么更新最短路树上到达结点p[i]的边在edges数组中的序号, p中记录终点 graph中的序号
- 更新源点到该终点的残量
+那么更新最短路上到达结点p[i]的边在edges数组中的序号, p中记录终点在graph中的序号
+ 更新源点到该终点的残量, 再把终点压入队列继续判断从这个点出发的所有边,
+ 直到终点的残量不为零或者没有终点在队列中.
  3.
 
 在每一次搜索完成之后，如果a[n]==0，说明已经没有剩余可以流通的量了，直接return
  */
-int MaxFlow(int s,int t){//最大流算法,s为源点,
+int MaxFlow(int s,int t);
+
+int MaxFlow(int s, int t) {//最大流算法,s为源点,
+    int a[1005],p[1005];//a数组表示源点到结点a[i]的残量,p数组表示最短路树上到达结点p[i]的边在edges数组中的序号
+    int flow=0;//最大流量
+
     return 0 ;
 }
+
+void insertEdge(string &from, string &to, int cap) {//插入边，起点为from，终点是to，容量为cap
+
+}
+
 int main(){
     string from,to;
     int n,cap;

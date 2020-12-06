9faaf7053f2fcead5ec0a99c26570d4b168cfde5
LukeLIN-web <1263810658@qq.com>
2020-12-06 22:44:08 +0800

    12.6 my first clion program, a network max flow proble spend my  15 -22:43  ,maybe 5 hours .

---
+++ DataStructure/dsHomework/graph/cmake-build-debug/CMakeFiles/graph.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graph.dir/main.cpp.obj"
+  "graph.exe"
+  "graph.exe.manifest"
+  "graph.lib"
+  "graph.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

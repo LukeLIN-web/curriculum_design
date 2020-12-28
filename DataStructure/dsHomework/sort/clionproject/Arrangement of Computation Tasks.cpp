153dba313c45b3f4ac965a4d43ba665b80fbb0a8
LukeLIN-web <1263810658@qq.com>
2020-12-28 23:29:37 +0800

    project 3 hard arrangement of computation tasks, sorted finished,we have sorted each vector in map

--- DataStructure/dsHomework/sort/clionproject/Arrangement of Computation Tasks.cpp
+++ DataStructure/dsHomework/sort/clionproject/Arrangement of Computation Tasks.cpp
@@ -44,7 +44,9 @@ bool comp(const time &a, const time &b){
         else return a.seconds < b.seconds;
     } else return true;
 }
-
+int timeSub(const time &a, const time &b){   // a should later than  b
+    return (a.hours-b.hours)*3600+(a.minutes-b.minutes)*60+(a.seconds-b.seconds);
+}
 class server{  //every pair
 public:
     server(time start,time end,string name);
@@ -85,33 +87,51 @@ int main(){
             }
         }
     }
-//    for(auto it : m1){ // we can use iterator
-//        vector<time>vt = it.second; // sort default from small to big , sort finished in 12.28 17:32
-//        cout << "===========before sort================" << endl;
-//        for (auto & it1 : vt){
-//            cout << "hour: " << it1.hours << " min: " << it1.minutes << endl;
-//        }
-//        sort(vt.begin(),vt.end(),comp);//define a customer compare rule
-//        cout << "===========after sort================" << endl;
-//        for (auto & it2 : vt){
-//            cout << "hours: " << it2.hours << " min: " << it2.minutes << endl;
-//        } //sort(it.second.begin(),it.second.end());
+    // it is difficult to change the vector in the map
+    map<string,std::vector<time>>::iterator mapIt ;// define iterator as a global variable for change data
+    std::vector<time> * vt;//pointer for change data
+    for(mapIt = m1.begin();mapIt != m1.end();mapIt++)
+    {
+        vt = & mapIt->second;
+        sort(vt->begin(),vt->end(),comp);//define a customer compare rule
+    }
+    // sort finished
+ // we can use iterator,notice we need change value so don't use const iterator
+ // sort default from small to big , sort finished in 12.28 23:26, we can
+//        sort(vt->begin(),vt->end(),comp);//define a customer compare rule
 //        // sort method actually does not take the number corresponding to the last address.
 //        // vector，set，map,The values taken out by the end() of these containers are not actually the last values
-//    }
+    for(auto it : m1){
+        vector<time>vt = it.second;
+        for (auto & it2 : vt){
+            cout << "hours: " << it2.hours << " min: " << it2.minutes << endl;
+        } //sort(it.second.begin(),it.second.end());
+    }
     // output the longest.
     //first we write down the beginning time of each server .
     // how to deal with switch from different servers?
     set<server> ser1;// store all the time block, each server has <start,end>
     for(auto it : m1){// move all into set. they will be sorted automatically.
         int count = it.second.size();
-        for (int i = 0; i < count;i += 2){
+        for (int i = 0; i+1 < count;i += 2){
             cout << "hour: " << it.second[i].hours << " min: " << it.second[i].minutes << endl;// for debug
             ser1.insert(server(it.second[i], it.second[i + 1], it.first) );// insert into set
         }
     }
     //In fact, there is no need to distinguish between servers in the set, we can save first
-
+    auto it = ser1.begin(); // traverse the set.
+    auto itNext = std::next(it);// find next the beginning time .
+    int tempT = 0;// tempT store current length of time.
+    int maxT = 0;
+    while (comp(itNext->start, it->end) && itNext != ser1.end() ) {  // if there is a block "itNext" start before the "it" end,
+            tempT += timeSub(itNext->start, it->start);//tempT =
+            it++;
+            itNext = std::next(it);
+        cout << "hour: " << it->start.hours << " min: " << it->start.minutes << endl;
+    }// if don't have any block can continue
+    tempT += timeSub(it->start, it->end);
+    maxT = tempT;
+    printf("%d\n",maxT);// the longest computation task you could run
     for(i = 0;i < K;i++ ){  // input the queries
         cin >> hours;
         cin >> dev; // enter ':'
